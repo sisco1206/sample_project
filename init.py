995bddeaba117589bb1a4cae8df6cc7e4f71a563
Francis Nahounou <programmingfnahounou6@gmail.com>
2024-04-04 19:12:58 -0500

    cherry-pick commit

--- init.py
+++ init.py
@@ -1,4 +1,8 @@
 def home_cars(model, brand):
     print(f" My new {model} will be the best {brand} cars in all the deals")
 
+<<<<<<< HEAD
 home_cars("Mercedes", "SLK")
+=======
+home_cars("Ford", "Escort") 
+>>>>>>> a9e29d9 (new code line)
