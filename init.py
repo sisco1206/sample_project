5ddb99a70c1599fa8a6d845f078b66aa596224f8
sisco1206 <97121785+sisco1206@users.noreply.github.com>
2024-04-10 16:35:11 -0500

    Update init.py
    updated code line

--- init.py
+++ init.py
@@ -1,8 +1,4 @@
 def home_cars(model, brand):
-    print(f" My new {model} will be the best {brand} cars in all the deals")
-
-<<<<<<< HEAD
-home_cars("Mercedes", "SLK")
-=======
-home_cars("Ford", "Escort") 
->>>>>>> a9e29d9 (new code line)
+    print(f" My new {model} will be the best {brand} cars in all the deals
+    
+home_cars("Ford", "Escort")
